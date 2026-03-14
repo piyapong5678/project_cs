0fdd455dea9f72f4550bd87f059da7742041e80b
piyapong5678 <kubya76@gmail.com>
2026-03-14 19:38:49 +0700

    router

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
@@ -6,12 +5,12 @@ import { RegisterComponent } from '../register/register.component';
 import { LoginDataModel } from '../shared/models/loginData.model';
 import { HttpClient } from '@angular/common/http';
 import { APP_CONFIG } from '../shared/constants/constants';
-import Swal from 'sweetalert2'; 
-import { Router } from '@angular/router'
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
@@ -21,11 +20,18 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
   }
 
-  constructor(private _dilog: MatDialog, private http: HttpClient,private router: Router) { }
+  constructor(
+    private _dilog: MatDialog,
+    private http: HttpClient,
+    private router: Router,
+  ) {}
 
   openRegisterEmForm() {
     const dialogRef = this._dilog.open(RegisterComponent, {
@@ -35,61 +41,57 @@ export class LoginComponent implements OnInit {
 
   hide = true;
 
-onLogin() {
-  const obj = {
-    email_user: this.loginForm.value.email,
-    password_user: this.loginForm.value.password,
-  };
+  onLogin() {
+    const obj = {
+      email_user: this.loginForm.value.email,
+      password_user: this.loginForm.value.password,
+    };
 
-  this.http.post(this.urlbackend + '/api/v1/login', obj).subscribe({
-    next: (res: any) => {
-  
-      if (res.id_user == null) {
-        sessionStorage.clear();
+    this.http.post(this.urlbackend + '/api/v1/login', obj).subscribe({
+      next: (res: any) => {
+        if (res.id_user == null) {
+          sessionStorage.clear();
+          Swal.fire({
+            title: 'ไม่พบบัญชีผู้ใช้!',
+            text: 'ไม่พบอีเมลนี้ในระบบ กรุณาตรวจสอบอีกครั้ง',
+            icon: 'warning',
+            confirmButtonText: 'ตกลง',
+          });
+        } else if (res.status_login) {
+          sessionStorage.setItem('user', JSON.stringify(res));
+
+          Swal.fire({
+            title: 'เข้าสู่ระบบสำเร็จ!',
+            text:
+              'ยินดีต้อนรับคุณ ' +
+              (res.name_user ? res.name_user : 'ผู้ใช้งาน'),
+            icon: 'success',
+            timer: 1500,
+            showConfirmButton: false,
+          }).then(() => {
+            this.router.navigate(['home']).then(() => {
+              window.dispatchEvent(new Event('cartUpdated')); // หรือสร้าง Event ใหม่ชื่อ userLoggedIn ก็ได้ครับ
+            });
+          });
+        } else {
+          sessionStorage.clear();
+          Swal.fire({
+            title: 'รหัสผ่านผิด!',
+            text: 'กรุณาตรวจสอบรหัสผ่านของคุณใหม่อีกครั้ง',
+            icon: 'error',
+            confirmButtonText: 'ลองใหม่',
+          });
+        }
+      },
+      error: (err) => {
         Swal.fire({
-          title: 'ไม่พบบัญชีผู้ใช้!',
-          text: 'ไม่พบอีเมลนี้ในระบบ กรุณาตรวจสอบอีกครั้ง',
-          icon: 'warning',
-          confirmButtonText: 'ตกลง'
-        });
-
-
-      } else if (res.status_login) {
-  
-        sessionStorage.setItem('user', JSON.stringify(res));
-
-        Swal.fire({
-          title: 'เข้าสู่ระบบสำเร็จ!',
-          text: 'ยินดีต้อนรับคุณ ' + (res.name_user ? res.name_user : 'ผู้ใช้งาน'),
-          icon: 'success',
-          timer: 1500,
-          showConfirmButton: false
-        }).then(() => {
-
-          this.router.navigate(['home']); 
-        });
-
-      
-      } else {
-        sessionStorage.clear();
-        Swal.fire({
-          title: 'รหัสผ่านผิด!',
-          text: 'กรุณาตรวจสอบรหัสผ่านของคุณใหม่อีกครั้ง',
+          title: 'เกิดข้อผิดพลาด!',
+          text: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้ในขณะนี้',
           icon: 'error',
-          confirmButtonText: 'ลองใหม่'
+          confirmButtonText: 'ตกลง',
         });
-      }
-    },
-    error: (err) => {
-      
-      Swal.fire({
-        title: 'เกิดข้อผิดพลาด!',
-        text: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้ในขณะนี้',
-        icon: 'error',
-        confirmButtonText: 'ตกลง'
-      });
-      console.error("Login Error:", err);
-    }
-  });
-}
+        console.error('Login Error:', err);
+      },
+    });
+  }
 }
